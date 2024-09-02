4685c25e228529e8053e6f682eb0dd6a2b84ffa3
pivnati <natanmuleta911@gmail.com>
2024-09-01 23:46:24 -0700

    create the  modify the registration form

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -6,17 +6,17 @@
 <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
     <form class="w-100"  style="max-width: 500px;">
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <label  class="form-label">Full name</label>
+            <input type="text" class="form-control" name="name" >
+          
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" name="email"  >
         </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <div class="mb-3">
+            <label  class="form-label">Password</label>
+            <input type="password" class="form-control" >
         </div>
         <button type="submit" class="btn btn-primary w-100">Submit</button>
     </form>
