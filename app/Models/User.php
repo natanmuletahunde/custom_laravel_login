c1a63aa6ddf3424b34ecba583060436bb3cccac6
pivnati <natanmuleta911@gmail.com>
2024-09-02 12:37:04 -0700

    install the composer require laravel/sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,8 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable , HasApiTokens;
+    use HasApiTokens, HasFactory, Notifiable;
+
     protected $table = 'users';
 
     /**
@@ -20,7 +21,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email', 
+        'email',
         'password',
     ];
 
@@ -35,15 +36,12 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
