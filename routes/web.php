3beea4353a0f5231e25dc52ce94c81c80355dce9
pivnati <natanmuleta911@gmail.com>
2024-09-02 14:55:19 -0700

    modify the web.php

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,7 @@ Route::get('/login', [AuthManager::class, 'login'])->name('login');
 Route::post('/login', [AuthManager::class, 'loginPost'])->name('login.post');
 Route::get('/registration', [AuthManager::class, 'registration'])->name('registration');
 Route::post('/registration', [AuthManager::class, 'registrationPost'])->name('registration.post');
+Route::get('/logout', [AuthManager::class, 'logout'])->name('logout');
+
+
 
