01b310e0661fa4695897b72c3eed1ee6aa2497f2
pivnati <natanmuleta911@gmail.com>
2024-09-02 15:22:15 -0700

    modify the create the has error and has success in the login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,23 @@
 
 @section('content')
 <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+<div class="mt-5">
+        @if ($errors->any())
+            <div class="col-12">
+            @foreach ($errors->all() as $errors)
+                <div class="alert alert-danger">{{$errors}}</div>
+            @endforeach
+            </div>
+        @endif
+
+         @if($session()->has('errors'))
+         <div class="alert alert-danger">{{session('error')}}</div> 
+          @endif
+           
+          @if($session()->has('success'))
+         <div class="alert alert-success">{{session('success')}}</div> 
+          @endif
+    </div>
     <form  action="{{route('login.post')}}" method="POST" class="w-100"  style="max-width: 500px;">
     @csrf   
          <div class="mb-3">
