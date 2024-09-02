b605dabf573b254e3b6d6d3cf0561983d4c10069
pivnati <natanmuleta911@gmail.com>
2024-09-02 15:33:27 -0700

    modify the registration form

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,40 +1,39 @@
 @extends('layout')
 
-@section('title','Registration')
+@section('title', 'Registration')
 
 @section('content')
 <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
     <div class="mt-5">
         @if ($errors->any())
             <div class="col-12">
-            @foreach ($errors->all() as $errors)
-                <div class="alert alert-danger">{{$errors}}</div>
-            @endforeach
+                @foreach ($errors->all() as $error)
+                    <div class="alert alert-danger">{{ $error }}</div>
+                @endforeach
             </div>
         @endif
 
-         @if($session()->has('errors'))
-         <div class="alert alert-danger">{{session('error')}}</div> 
-          @endif
+        @if(session()->has('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div> 
+        @endif
            
-          @if($session()->has('success'))
-         <div class="alert alert-success">{{session('success')}}</div> 
-          @endif
+        @if(session()->has('success')) // seession is method in the laravel 
+            <div class="alert alert-success">{{ session('success') }}</div> 
+        @endif
     </div>
-    <form   action="{{route('registration.post')}}" method="POST" class="w-100"  style="max-width: 500px;">
-    @csrf
+    <form action="{{route('registration.post')}}" method="POST" class="w-100" style="max-width: 500px;">
+        @csrf
         <div class="mb-3">
-            <label  class="form-label">Full name</label>
-            <input type="text" class="form-control" name="name" >
-          
+            <label class="form-label">Full name</label>
+            <input type="text" class="form-control" name="name">
         </div>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" name="email"  >
+            <label class="form-label">Email address</label>
+            <input type="email" class="form-control" name="email">
         </div>
         <div class="mb-3">
-            <label  class="form-label">Password</label>
-            <input type="password" class="form-control"  name="password">
+            <label class="form-label">Password</label>
+            <input type="password" class="form-control" name="password">
         </div>
         <button type="submit" class="btn btn-primary w-100">Submit</button>
     </form>
